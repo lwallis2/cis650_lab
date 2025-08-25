ee441e780c0b0e491abef57776df600912f81b23
Luc Wallis <wallisl@students.uni-marburg.de>
2025-08-25 18:11:35 +0200

    Done with task 4

---
+++ Lab1-Statements/task4.py
@@ -0,0 +1,8 @@
+miles_driven = input("Enter of miles driven: ")
+gallons_used = input("Enter numver of gallons used: ")
+if miles_driven.isdigit() and gallons_used.isdigit():
+    miles_driven = int(miles_driven)
+    gallons_used = float(gallons_used)
+    print("Miles per gallon: ", miles_driven/gallons_used)
+else: 
+    print("Invalid Input! Please enter a number")
