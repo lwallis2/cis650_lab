fcc6225d2a8079bcef4129e37d95f2b1a5d3f523
Luc Wallis <wallisl@students.uni-marburg.de>
2025-08-25 18:03:08 +0200

    Done with task 3

---
+++ Lab1-Statements/task3.py
@@ -0,0 +1,7 @@
+first_name = input('What is your fist name')
+last__name = input("What is your last name")
+
+if len(first_name) > 0 and len(last__name) > 0: 
+    print ('Hello', first_name, last__name)
+else:
+    print("Please enter your full name!")
