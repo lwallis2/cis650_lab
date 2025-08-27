e2c2954b2b0fbec58ef6f1902bd46a22aa7b3587
Luc Wallis <wallisl@students.uni-marburg.de>
2025-08-27 17:57:52 +0200

    Added the rounding feature

--- Lab1-Statements/task2.py
+++ Lab1-Statements/task2.py
@@ -1,4 +1,4 @@
-miles_driven = int(input("Enter of miles driven: "))
+miles_driven = float(input("Enter of miles driven: "))
 gallons_used = float(input("Enter numver of gallons used: "))
-miles_per_gallon = miles_driven/gallons_used
+miles_per_gallon = round(miles_driven/gallons_used, 4)
 print("Miles per gallon: ", miles_per_gallon)
