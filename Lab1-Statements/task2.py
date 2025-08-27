6507ab725ca2500a5686ae391687e81da12e45ac
Luc Wallis <wallisl@students.uni-marburg.de>
2025-08-27 17:47:59 +0200

    Adding sth to task 2

--- Lab1-Statements/task2.py
+++ Lab1-Statements/task2.py
@@ -1,3 +1,4 @@
 miles_driven = int(input("Enter of miles driven: "))
 gallons_used = float(input("Enter numver of gallons used: "))
-print("Miles per gallon: ", miles_driven/gallons_used)
+miles_per_gallon = miles_driven/gallons_used
+print("Miles per gallon: ", miles_per_gallon)
