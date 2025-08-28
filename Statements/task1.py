cd2101a7e92dbe1f69c57e5b3d6c0b2b4618fa10
Luc Wallis <wallisl@students.uni-marburg.de>
2025-08-28 03:27:26 +0200

    Done with task 1

---
+++ Statements/task1.py
@@ -0,0 +1,23 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+my_name = "Luc"
+
+#Check if name ist blank
+if name == "":
+    print("Name cannot be blank.")
+#Check if age is a number
+elif not age.isdigit():
+    print("Age has to be a number")
+else: 
+    age = int(age)
+    if name == my_name:
+        if age <= 0:
+            print("You are not born yet.")
+        elif age < 18:
+            print('You are a minor')
+        elif age <= 99:
+            print("Adult")
+        elif age > 99:
+            print("Nice to meet you ")
+    else: 
+        print (f'{name} is not my Name ({my_name})')
